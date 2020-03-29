76944f29c011454e17b2c3cca7dc2657308347b2
Iosif.Barta <iosif.barta@gmail.com>
2020-03-29 14:39:35 +0300

    Operarea cu conceptul de supraîncărcare a metodelor

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -13,9 +13,13 @@ public class App
 
         OperatiiMatematice operatii = new OperatiiMatematice();
         operatii.adunarea(14, 7);
+        operatii.adunarea(14.00, 7.00);
         operatii.scaderea(14,7);
+        operatii.scaderea(13.00, 12.00);
         operatii.inmultirea(14,7);
-        operatii.impartirea(14,7);
+        operatii.inmultirea(8.00, 9.5);
+        operatii.impartirea(15,7);
+        operatii.impartirea(15.5, 2.8);
 
 
 
--- src/main/java/org/fasttrackit/OperatiiMatematice.java
+++ src/main/java/org/fasttrackit/OperatiiMatematice.java
@@ -9,9 +9,20 @@ public class OperatiiMatematice {
         System.out.println("adunarea: " + number1 + " + " + number2 + " = " + num);
         return num;
     }
+    public double adunarea(double number1, double number2){
+        double num = number1 + number2;
+        System.out.println("adunarea: " + number1 + " + " + number2 + " = " + num);
+        return num;
+    }
+
 
     public int scaderea(int number1, int number2) {
         int num = number1 - number2;
+        System.out.println("scaderea: " + number1 + " - " + number2 + " = " + num);
+        return num;
+    }
+    public double scaderea(double number1, double number2) {
+        double num = number1 - number2;
         System.out.println("scaderea: " + number1 + " - " + number2 + " = " + num);
         return num;
     }
@@ -21,8 +32,19 @@ public class OperatiiMatematice {
         System.out.println("inmultirea: " + number1 + " * " + number2 + " = " + num);
         return num;
     }
+    public double inmultirea(double number1, double number2) {
+        double num = number1 * number2;
+        System.out.println("inmultirea: " + number1 + " * " + number2 + " = " + num);
+        return num;
+    }
+
     public double impartirea(double number1, double number2) {
         double num = number1 / number2;
+        System.out.println("impartirea: " + number1 + " / " + number2 + " = " + num);
+        return num;
+    }
+    public int impartirea(int number1, int number2) {
+        int num = number1 / number2;
         System.out.println("impartirea: " + number1 + " / " + number2 + " = " + num);
         return num;
     }
